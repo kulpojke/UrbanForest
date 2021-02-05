22bac130e791db49669986cd5b94c59a9cd34612
michael <michaelhuggins@protonmail.com>
2021-02-05 19:24:44 +0000

    x

--- NAIP/naip.py
+++ NAIP/naip.py
@@ -5,7 +5,6 @@
 # NAIP imagery comes as mrSID.
 # Before running this convert to tif. Download the lizardtech converter from https://www.extensis.com/support/developers. Then download newist sdk. Extract it then  ```cd MrSID_DSDK-9.5.4.4709-rhel6.x86-64.gcc531/Raster_DSDK/bin/```. Then ```./mrsiddecode -wf -i infile.sid -o outfile.tif```
 
-#%%
 import os
 import matplotlib.pyplot as plt
 import numpy as np
@@ -17,7 +16,7 @@ import earthpy.spatial as es
 import xarray as xr
 
 
-def get_geotiff_info(infile, output='print'):
+def get_make_ndvi_args(infile, output):
     '''returns info about tiff'''
     with rio.open(infile) as dataset:
         width, height = dataset.shape
@@ -28,22 +27,23 @@ def get_geotiff_info(infile, output='print'):
         proj = dataset.crs
         gt = dataset.transform
         dtype =  metadata['dtype']
-        if output = 'print':
-            s = f'shape: {(width, height)}\ncount: {count}\n\n{desc}\n\nsrs: {proj}\ndtype: {dtype}'
+        splitfile = infile.split('.')
+        outfile = splitfile[0] + '_ndvi' + '.tif'
+        
+        if output == 'print':
+            s = f'''shape: {(width, height)}
+                    count: {count}
+                    srs: {proj}
+                    dtype: {dtype}
+                    description:
+                    {desc}
+                    
+                    dtype: {dtype}'''
+            print(s)
         else:
-            poo = {'width'        : width,
-                   'height'       : height,
-                   'count'        : count,
-                   'description'  : desc,
-                   'metadata'     : metadata,
-                   'driver'       : ddriver, 
-                   'crs'          : proj,
-                   'geottransorm' : gt,
-                   'dtype' : dtype
-                    }
-            return(poo)
+            return(infile, outfile, width, height, dtype, count, crs, driver)
 
-def _make_ndvi(infile, outfile, width, height, dtype, count, driver='GTiff', crs):
+def make_ndvi(infile, outfile, width, height, dtype, count, crs, driver='GTiff'):
     with rio.open(outfile, 'w', width=width, height=height, dtype=dtype, count=count, driver=driver, crs=crs) as dst:
         with rio.open(infile) as src:
             for i, window in src.block_windows(1):
@@ -51,6 +51,4 @@ def _make_ndvi(infile, outfile, width, height, dtype, count, driver='GTiff', crs
                 naip_ndvi = es.normalized_diff(naip_data[2], naip_data[0]) # 
                 naip_ndvi = naip_ndvi.astype(dtype) #TODO: will be three in real [3] - [2]
                 dst.write(naip_ndvi, window=window, indexes=1)
-# %%
 
-# %%
